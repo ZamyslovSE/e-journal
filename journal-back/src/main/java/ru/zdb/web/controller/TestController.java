998ca151afbee7e5afbf77147c6b41d2d178cad2
Stepan Zamyslov <zamyslov.stepan@gmail.com>
2020-06-25 23:02:39 +0300

    init

---
+++ journal-back/src/main/java/ru/zdb/web/controller/TestController.java
@@ -0,0 +1,18 @@
+package ru.zdb.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<String> hello(){
+        return new ResponseEntity<String>("hello", HttpStatus.OK);
+    }
+
+
+}
